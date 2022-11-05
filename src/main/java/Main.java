3f46131c578141963304ca508107ea80118d3f55
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2022-11-05 15:51:48 -0300

    Arquivo de dados do grafo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import model.Graph;
+import util.LoadData;
+
+public class Main {
+	public static void main(String[] args) {
+        Graph graphMatriz;
+
+        graphMatriz = LoadData.loadAdj("dijkstra\\src\\main\\java\\data\\data.txt");
+
+        System.out.println("Matriz:");
+        System.out.println(graphMatriz);
+
+        Graph lisGraph = LoadData.loadList("dijkstra\\src\\main\\java\\data\\data.txt");
+
+        System.out.println("Lista:");
+        System.out.println(lisGraph);
+    }
 }
+
--- src/main/java/util/LoadData.java
+++ src/main/java/util/LoadData.java
@@ -94,5 +94,4 @@ public class LoadData {
             return null;
         }
     }
-
 }
